43d242e9baeeabf54bd4a7228ea82e0098ef60a0
Shrouk Mamdouh <shrouk@contactless.io>
2021-03-19 10:01:35 +0200

    add nebular library to the app

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbIconModule, NbCardModule, NbButtonModule, NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NbEvaIconsModule,
+    NbIconModule,
+    NbCardModule,
+    NbButtonModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    HttpClientModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
