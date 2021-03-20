867f2b94633f7cd6ee586bd2cf117b95b0b2a365
Shrouk Mamdouh <shrouk@contactless.io>
2021-03-20 05:51:58 +0200

    implement add product method

--- app/src/app/modules/products/components/products-list/products-list.component.ts
+++ app/src/app/modules/products/components/products-list/products-list.component.ts
@@ -6,8 +6,9 @@ import {
 } from '@angular/core';
 import { NbDialogService, NbToastrService } from '@nebular/theme';
 import { ProductService } from '../../services/product.service';
-import * as products from '../../../../../assets/mocked-data/products.json';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products-list',
@@ -16,36 +17,65 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent implements OnInit {
-  products: any = (products as any).default;
+  products: any;
   form = new FormGroup({
     name: new FormControl('', [Validators.required]),
     price: new FormControl('', [Validators.required]),
-    code: new FormControl('', [Validators.required]),
+    code: new FormControl(''),
     desc: new FormControl(''),
     quantity: new FormControl('', [Validators.required]),
   });
   constructor(
     private productService: ProductService,
     private dialogService: NbDialogService,
-    private toastrService: NbToastrService
+    private toastrService: NbToastrService,
+    private http: HttpClient
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
 
   get f() {
     return this.form.controls;
   }
 
   submit() {
-    console.log('form data', this.form.value);
-  }
-
-  isFormValid(): boolean {
-    return this.form.invalid;
+    const data = this.products.data;
+    this.form.value.code = this.makeRandomCode()
+    if (this.form.valid) {
+      data.push(this.form.value);
+      this.products.data = data;
+      console.log('form data', this.products);
+      this.resetValue();
+    }
   }
   resetValue() {
-    console.log('fff');
-    this.form.reset({ name: '' });
+    this.form.reset({ name: '', price: '', quantity: '', code: '', desc: '' });
+  }
+
+  getProducts() {
+    this.http
+      .get('../../../../assets/mocked-data/products.json')
+      .pipe(
+        map((data) => {
+          console.log('result', data);
+          this.products = data;
+          return data;
+        })
+      )
+      .subscribe((res) => {});
+  }
+
+  makeRandomCode() {
+    let result = '';
+    let characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (let i = 0; i < 100; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
   openDialog(dialog: TemplateRef<any>) {
     this.dialogService.open(dialog);
--- app/src/app/modules/products/services/product.service.ts
+++ app/src/app/modules/products/services/product.service.ts
@@ -1,17 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpEvent, HttpErrorResponse, HttpEventType } from  '@angular/common/http';
-import { map } from  'rxjs/operators';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  SERVER_URL: string = "https://file.io/";
-  constructor(private httpClient: HttpClient) { }
-
-  public upload(formData) {
-    return this.httpClient.post<any>(this.SERVER_URL, formData, {
-        reportProgress: true,
-        observe: 'events'
-      });
+  constructor() {
   }
 }
